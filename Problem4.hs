496614342fbcdd59cf4039cba3e776a8b393486c
Michal Charemza <michalcharemza@gmail.com>
2014-11-02 22:05:54 +0000

    Problem 4

---
+++ Problem4.hs
@@ -0,0 +1,5 @@
+module Problem4 where
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
---
+++ Problem4Test.hs
@@ -0,0 +1,19 @@
+module Problem4Test where
+
+import Problem4
+import Test.HUnit
+import Common
+
+emptyListTest :: Test
+emptyListTest = TestCase $ assertEqual "for (myLength [])," 0 $ myLength []
+
+singleItemListTest :: Test
+singleItemListTest = TestCase $ assertEqual "for (myLength [[]])," 1 $ myLength [[]]
+
+twoItemListTest :: Test
+twoItemListTest = TestCase $ assertEqual "for (myLength [[],[]])," 2 $ myLength [[],[]]
+
+tests = TestList [emptyListTest, singleItemListTest, twoItemListTest]
+
+main :: IO Counts
+main = runTestTT $ tests
