1df86a54aa9b7fa61da0834bd9ae30785214485e
Michal Charemza <michalcharemza@gmail.com>
2014-11-03 20:36:42 +0000

    Problem 5

---
+++ Problem5.hs
@@ -0,0 +1,6 @@
+module Problem5 where
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse [x] = [x]
+myReverse (x:xs) = (myReverse xs) ++ [x]
---
+++ Problem5Test.hs
@@ -0,0 +1,23 @@
+module Problem5Test where
+
+import Problem5
+import Test.HUnit
+import Common
+
+-- Empty list needs a type for assertEqual
+data Dummy = Dummy
+	deriving(Eq, Show)
+
+emptyList :: [Dummy]
+emptyList = []
+
+emptyListTest :: Test
+emptyListTest = TestCase $ assertEqual "for (myReverse [])," emptyList $ myReverse emptyList
+
+threeItemListTest :: Test
+threeItemListTest = TestCase $ assertEqual "for (myReverse [1,2,3])," [3,2,1] $ myReverse [1,2,3]
+
+tests = TestList [emptyListTest, threeItemListTest]
+
+main :: IO Counts
+main = runTestTT $ tests
