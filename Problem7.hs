dc32943813b88a02554a71a3ba713e7bfe82a72a
Michal Charemza <michalcharemza@gmail.com>
2014-11-05 23:36:48 +0000

    Problem 7

---
+++ Problem7.hs
@@ -0,0 +1,7 @@
+module Problem7 where
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
---
+++ Problem7Test.hs
@@ -0,0 +1,13 @@
+module Problem7Test where
+
+import Problem7
+import Test.HUnit
+
+
+nestedListTest :: Test
+nestedListTest = TestCase $ assertEqual "for (flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) )," [1,2,3,4,5] $ flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+
+tests = TestList [nestedListTest]
+
+main :: IO Counts
+main = runTestTT $ tests
